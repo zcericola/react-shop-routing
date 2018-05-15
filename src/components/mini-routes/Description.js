162f80972636a505eebe7566077a7a6eb45f01af
Bryce Hull <bahull@live.com>
2018-05-15 17:54:32 -0500

    Updating styling

--- src/components/mini-routes/Description.js
+++ src/components/mini-routes/Description.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const Description = ({ description }) => {
   return (
     <div>
+      <h3>Description:</h3>
       <h3>{description}</h3>
     </div>
   );
